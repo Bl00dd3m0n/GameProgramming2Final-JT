d0983b9187a27575a30daef5629d1c39a3a66d25
Detroit Blood <lboo300@gmail.com>
2020-05-13 23:28:22 -0500

    Messed up my endscreen generation

--- src/0x46696E616C/MainMenu/Game1.cs
+++ src/0x46696E616C/MainMenu/Game1.cs
@@ -94,7 +94,7 @@ namespace MainMenu
             {
                 GenerateStyleSheet("SettingsPage");
             }
-            if (File.Exists("EndScreen.ss"))
+            if (!File.Exists("EndScreen.ss"))
             {
                 GenerateStyleSheet("EndScreen");
             }
